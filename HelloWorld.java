71f299c963032e8b2d241421ee6c1af3152a3a7b
Daniel V <daniv103002@gmail.com>
2021-02-01 22:09:31 -0500

    Update HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 
-public calss HelloWorld {
+public class HelloWorld {
 
    public static void main(String[] args)
    {
       System.out.println("\n Hello World ");
+      System.out.println("\n Program modified, but able to revert to original version using Github Desktop. ");
    }
 
 }
