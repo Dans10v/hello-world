79f47b9faf6d92e9767ff4c0b7c0dead87d1a13f
Daniel V <daniv103002@gmail.com>
2021-02-01 22:13:36 -0500

    run HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
    public static void main(String[] args)
    {
       System.out.println("\n Hello World ");
-      System.out.println("\n Program modified, but able to revert to original version using Github Desktop. ");
    }
 
 }
